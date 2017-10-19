770a8c894c5e85f624294512db4580a92daf18aa
zhiaihao <1014504021@qq.com>
2017-10-19 14:00:52 +0800

    学工框架搭建

---
+++ vuexg/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <el-switch
+      v-model="value1"
+      on-text=""
+      off-text="">
+    </el-switch>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      value1: true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
