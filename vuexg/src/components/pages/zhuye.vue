a960c0870fc53ea9313518d159d810421d739bf2
zhiaihao <1014504021@qq.com>
2017-10-26 18:42:27 +0800

    主页 技术实践 ....

--- vuexg/src/components/pages/zhuye.vue
+++ vuexg/src/components/pages/zhuye.vue
@@ -2,32 +2,46 @@
   <div class="zhuye">
     <div class="lunbo">
       <mt-swipe :auto="4000">
-        <mt-swipe-item>1</mt-swipe-item>
-        <mt-swipe-item>2</mt-swipe-item>
+        <mt-swipe-item>1
+          <ul>
+            <li @click="alert">1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+          </ul>
+        </mt-swipe-item>
+        <mt-swipe-item v-if="ispageshow">2</mt-swipe-item>
         <mt-swipe-item>3</mt-swipe-item>
       </mt-swipe>
     </div>
+
 
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import lunbo from '@/components/lunbo'
 import { Swipe, SwipeItem } from 'mint-ui'
 
 Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
 export default {
   name: 'zhuye',
+  data () {
+    return {
+      ispageshow: false
+    }
+  },
   mounted: function () {
     console.log('zy')
     this.$emit('mountedcomplete')
   },
   methods: {
+    alert: function () {
+      alert('ajsadfvujf')
+    }
   },
   components: {
-    lunboview: lunbo
   }
 }
 </script>
@@ -39,4 +53,10 @@ export default {
     height:180px;
     background-color:red;
   }
+ li{
+   display: inline-block;
+   background-color: darkgreen;
+   width:50px;
+   height:50px;
+ }
 </style>
